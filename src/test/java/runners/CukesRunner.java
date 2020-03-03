0abc5306cd8ddf9d3e26dc04625826035af4705b
Norbert <https://github.com/norbertBirsan/VeryFirst.git>
2020-03-02 21:17:36 -0600

    Hi Guys

---
+++ src/test/java/runners/CukesRunner.java
@@ -0,0 +1,21 @@
+package runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json",
+                "junit:target/cucumber-reports/Cucumber.xml",
+                "html:target/cucumber-reports"},
+        features = "src/test/resources/features/",
+        glue = "step_definitions",
+        dryRun = true,
+        tags = ""   // we can pass more than one tag, ex: tags = "@smokeTest, @regressionTest"
+)
+
+
+public class CukesRunner {
+
+}
---
+++ src/test/java/step_definitions/Hooks.java
@@ -0,0 +1,35 @@
+package step_definitions;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.Config;
+import utilities.Driver;
+
+public class Hooks {
+
+
+    @Before
+    public void setUp(){
+        //Anything you want to run before each scenario
+        Driver.getDriver().get(Config.getProperty("trainingUrl"));
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+
+        if(scenario.isFailed()){
+
+            byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+
+        }
+        Driver.quitDriver();
+
+    }
+    
+
+
+}
