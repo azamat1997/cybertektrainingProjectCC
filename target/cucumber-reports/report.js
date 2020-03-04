24f0ad9ec700c127a8db75b8ca5dd5f820f3456e
Anvar Sono <kerimbekovich89@gmail.com>
2020-03-04 13:11:02 -0600

    up

---
+++ target/cucumber-reports/report.js
@@ -0,0 +1,406 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/features/1 (1).feature");
+formatter.feature({
+  "name": "",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@COD-279"
+    }
+  ]
+});
+formatter.scenarioOutline({
+  "name": "Add new student functionality",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@COD-273"
+    }
+  ]
+});
+formatter.step({
+  "name": "User click on Students module",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "User click on Add student",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cFirstname\u003e\" in first name",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cLastname\u003e\" in last name",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cEmail\u003e\" in email",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cJoining Date\u003e\" in joining date",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cPassword\u003e\" in password",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cComfirm Password\u003e\" in comfirm password",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cSubject\u003e\" in subject",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cMobile number\u003e\" in mobile number",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cGender\u003e\" in gender",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cAdmission No\u003e\" in admission No",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cBirth Date\u003e\" in birth date",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cMajor\u003e\" in major",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cBatch\u003e\" in batch",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cSection\u003e\" in section",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cPresent Address\u003e\" present address",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cCompany Name\u003e\" compamy name",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cTitle\u003e\" in title",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cStart Date\u003e\" start date",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cCity\u003e\" in city",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cStreet (123 N. California, Apt 1)\u003e\" in street",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cZipCode\u003e\" in zipcode",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User enter \"\u003cState\u003e\" in state",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User click to the Submit button",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "Firstname",
+        "Lastname",
+        "Email",
+        "Joining Date",
+        "Password",
+        "Comfirm Password",
+        "Subject",
+        "Mobile number",
+        "Gender",
+        "Admission No",
+        "Birth Date",
+        "Major",
+        "Batch",
+        "Section",
+        "Present Address",
+        "Company Name",
+        "Title",
+        "Start Date",
+        "City",
+        "Street (123 N. California, Apt 1)",
+        "ZipCode",
+        "State"
+      ]
+    },
+    {
+      "cells": [
+        "John",
+        "Johnes",
+        "johnjohnes@gmail.com",
+        "03/10/2018",
+        "12345",
+        "12345",
+        "tttt",
+        "312312777",
+        "male",
+        "123",
+        "12/12/1989",
+        "fighter",
+        "1",
+        "2",
+        "123 chicago street",
+        "Jonh inc",
+        "fighter",
+        "02/02/2020",
+        "chicago",
+        "1223 chicago street",
+        "60659",
+        "IL"
+      ]
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Add new student functionality",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@COD-279"
+    },
+    {
+      "name": "@COD-273"
+    }
+  ]
+});
+formatter.before({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "User click on Students module",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "CreateStudent_StepDefs.user_click_on_Students_module()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "User click on Add student",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CreateStudent_StepDefs.user_click_on_Add_student()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "User enter \"John\" in first name",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"Johnes\" in last name",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"johnjohnes@gmail.com\" in email",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"03/10/2018\" in joining date",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"12345\" in password",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"12345\" in comfirm password",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"tttt\" in subject",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"312312777\" in mobile number",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"male\" in gender",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"123\" in admission No",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"12/12/1989\" in birth date",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"fighter\" in major",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"1\" in batch",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"2\" in section",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"123 chicago street\" present address",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"Jonh inc\" compamy name",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"fighter\" in title",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"02/02/2020\" start date",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"chicago\" in city",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"1223 chicago street\" in street",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"60659\" in zipcode",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enter \"IL\" in state",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User click to the Submit button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "CreateStudent_StepDefs.user_click_to_the_Submit_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "status": "skipped"
+});
+});
