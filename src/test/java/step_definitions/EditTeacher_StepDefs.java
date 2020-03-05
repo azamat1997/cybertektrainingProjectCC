0b1f1df2c5bf2c9becb3e1f5fb9a88906a7ad349
Eduard Enache <enacheeduardc@gmail.com>
2020-03-05 14:28:25 -0600

    ed's project

--- src/test/java/step_definitions/EditTeacher_StepDefs.java
+++ src/test/java/step_definitions/EditTeacher_StepDefs.java
@@ -1,83 +1,186 @@
 package step_definitions;
 
+import cucumber.api.java.bs.A;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.eo.Se;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import pages.AllTeachersPage;
+import pages.EditTeacherPage;
+import pages.HomePage;
+import utilities.DBUtility;
+import utilities.Driver;
+import utilities.SeleniumUtils;
+import utilities.TempStorage;
+
+import javax.swing.*;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 public class EditTeacher_StepDefs {
 
-    @When("User click on {int}dots button on Christian teacher")
-    public void user_click_on_dots_button_on_Christian_teacher(Integer int1) {
+    HomePage homePage = new HomePage();
+    EditTeacherPage editTeacherPage = new EditTeacherPage();
+    AllTeachersPage allTeachersPage = new AllTeachersPage();
 
+    @When("User click on threedots button on Christian teacher")
+    public void user_click_on_threedots_button_on_Christian_teacher() {
+        homePage.teachersModuleButton.click();
+        homePage.allTeacherButton.click();
+        SeleniumUtils.pause(2);
+        editTeacherPage.threeDotsDropdown.click();
+        SeleniumUtils.pause(3);
+
+    }
+
+    @When("User click on Edit button")
+    public void userClickOnEditButton() {
+        editTeacherPage.editbutton.click();
+        SeleniumUtils.pause(2);
     }
 
     @When("User edit {string} in first name")
-    public void user_edit_in_first_name(String string) {
-
+    public void user_edit_in_first_name(String FirstnameValue) {
+        SeleniumUtils.pause(2);
+        // ChrChen id546
+        editTeacherPage.inputFirstname.clear();
+        TempStorage.addData("teacher_FirstName", FirstnameValue);
+    editTeacherPage.inputFirstname.sendKeys(FirstnameValue);
     }
 
     @When("User edit {string} in last name")
-    public void user_edit_in_last_name(String string) {
-
+    public void user_edit_in_last_name(String LastnameValue) {
+        SeleniumUtils.pause(2);
+        editTeacherPage.inputLastname.clear();
+    editTeacherPage.inputLastname.sendKeys(LastnameValue);
     }
 
     @When("User edit {string} in email")
-    public void user_edit_in_email(String string) {
-
+    public void user_edit_in_email(String EmailValue) {
+        SeleniumUtils.pause(2);
+        editTeacherPage.inputEmail.clear();
+    editTeacherPage.inputEmail.sendKeys(EmailValue);
     }
 
     @When("User edit {string} in joining date calendar")
-    public void user_edit_in_joining_date_calendar(String string) {
+    public void user_edit_in_joining_date_calendar(String CalendarValue) {
+        SeleniumUtils.pause(2);
+        editTeacherPage.inputJoiningdate.sendKeys(CalendarValue);
+
 
     }
 
     @When("User edit {string} in password")
-    public void user_edit_in_password(String string) {
-
+    public void user_edit_in_password(String passwordValue) {
+        SeleniumUtils.pause(2);
+        editTeacherPage.inputFirstname.clear();
+    editTeacherPage.inputPassword.sendKeys(passwordValue);
     }
 
     @When("User edit {string} in subject")
-    public void user_edit_in_subject(String string) {
-
+    public void user_edit_in_subject(String subjectValue) {
+        SeleniumUtils.pause(2);
+        editTeacherPage.subjectTextBox.clear();
+        editTeacherPage.subjectTextBox.sendKeys(subjectValue);
     }
 
     @When("User edit {string} in mobile number")
-    public void user_edit_in_mobile_number(String string) {
-
+    public void user_edit_in_mobile_number(String mobileNumberValue) {
+        SeleniumUtils.pause(2);
+        editTeacherPage.mobileNumberBox.clear();
+    editTeacherPage.mobileNumberBox.sendKeys(mobileNumberValue);
     }
 
     @When("User edit {string} in gender")
-    public void user_edit_in_gender(String string) {
+    public void user_edit_in_gender(String genderValue) {
+        SeleniumUtils.waitForVisibility(editTeacherPage.birthDateInput, 5);
+    Select select = new Select( editTeacherPage.genderInput);
+    select.selectByVisibleText(genderValue);
 
     }
 
     @When("User edit {string} in department dropdown")
-    public void user_edit_in_department_dropdown(String string) {
-
+    public void user_edit_in_department_dropdown(String departmentDropdown) {
+        SeleniumUtils.waitForVisibility(editTeacherPage.birthDateInput, 5);
+    Select select = new Select(editTeacherPage.departmentInput);
+    select.selectByVisibleText(departmentDropdown);
     }
 
     @When("User edit {string} in birth date calendar")
-    public void user_edit_in_birth_date_calendar(String string) {
-
+    public void user_edit_in_birth_date_calendar(String birthDateValue) {
+        SeleniumUtils.waitForVisibility(editTeacherPage.birthDateInput, 5);
+        editTeacherPage.birthDateInput.sendKeys(birthDateValue);
     }
 
     @When("User edit {string} in salary")
-    public void user_edit_in_salary(String string) {
-
+    public void user_edit_in_salary(String salaryValue) {
+        SeleniumUtils.pause(2);
+        editTeacherPage.salaryInput.clear();
+    editTeacherPage.salaryInput.sendKeys(salaryValue);
     }
 
     @When("User edit {string} in batch")
-    public void user_edit_in_batch(String string) {
-
+    public void user_edit_in_batch(String batchValue) {
+        SeleniumUtils.pause(2);
+        editTeacherPage.batchInput.sendKeys(batchValue);
     }
 
     @When("User edit {string} in section")
-    public void user_edit_in_section(String string) {
-
+    public void user_edit_in_section(String sectionValue) {
+        SeleniumUtils.pause(2);
+        editTeacherPage.sectionInput.sendKeys(sectionValue);
     }
 
     @When("User edit {string} in present adress")
-    public void user_edit_in_present_adress(String string) {
+    public void user_edit_in_present_adress(String adressValue) {
+        SeleniumUtils.pause(2);
+        editTeacherPage.permanentAddressInput.clear();
+        editTeacherPage.permanentAddressInput.sendKeys(adressValue);
+    }
+
+    @When("User click Submit button")
+    public void user_click_Submit_button() {
+        SeleniumUtils.pause(2);
+       editTeacherPage.submitButton.click();
+        SeleniumUtils.pause(2);
+    }
+
+    @Then("User should see the edited profile displayed in the UI")
+    public void user_should_see_the_edited_profile_displayed_in_the_UI() {
+      //  Assert.assertTrue("Teacher's name is not found on this page", allTeachersPage.teacherFirstNameLocator(TempStorage.getData("teacher_FirstName")).isDisplayed());
+
+
+    }
+
+    @Then("Tester should be able to veryfy the edited Teacher in DataBase")
+    public void tester_should_be_able_to_veryfy_the_edited_Teacher_in_DataBase() {
+
+//        try {
+//            DBUtility.createConnection();
+//            List<Map<Object, Object>> data = DBUtility.executionQuery("select teacher_FirstName where teacher_id = 546 ");
+//            DBUtility.close();
+//
+//            for(Map<Object, Object> map: data){
+//                if(map.containsValue(TempStorage.getData("teacher_FirstName"))){
+//                    Assert.assertTrue("No matching record found. Verification FAILED", map.containsValue(TempStorage.getData("teacher_FirstName")));
+//                    return;
+//                }
+//                Assert.fail();
+//            }
+//
+//        }catch (SQLException e){
+//            e.printStackTrace();
+//        }
 
     }
 
 }
+
+//|Cocolero |Coco    |coco@aaa.com| 22/12/2020 | 44444   |   PP  | 5555555555 |Male  |English   | 11/11/2002 | 33000 |5    | Java   | 8thAvenue  |
--- target/cucumber-reports/report.js
+++ target/cucumber-reports/report.js
@@ -10,89 +10,89 @@ formatter.feature({
   ]
 });
 formatter.scenarioOutline({
-  "name": "Verify Create Teacher Functionality",
+  "name": "Verify Update Teacher functionality",
   "description": "",
   "keyword": "Scenario Outline",
   "tags": [
     {
-      "name": "@COD-278"
+      "name": "@COD-274"
     }
   ]
 });
 formatter.step({
-  "name": "User clicks on Teachers module",
+  "name": "User click on threedots button on Christian teacher",
   "keyword": "When "
 });
 formatter.step({
-  "name": "User clicks on Add Teacher",
+  "name": "User click on Edit button",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User enters \"\u003cFirst name\u003e\" in first name",
+  "name": "User edit \"\u003cFirstname\u003e\" in first name",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User enters \"\u003cLast name\u003e\" in last name",
+  "name": "User edit \"\u003cLastname\u003e\" in last name",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User enters \"\u003cEmail\u003e\" in email",
+  "name": "User edit \"\u003cEmail\u003e\" in email",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User enters \"\u003cJoining Date\u003e\" in joining date",
+  "name": "User edit \"\u003cJoiningDate\u003e\" in joining date calendar",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User enters \"\u003cPassword\u003e\" in password",
+  "name": "User edit \"\u003cPassword\u003e\" in password",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User enters \"\u003cSubject\u003e\" in subject",
+  "name": "User edit \"\u003cSubject\u003e\" in subject",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User enters \"\u003cMobile number\u003e\" in mobile number",
+  "name": "User edit \"\u003cMobileNumber\u003e\" in mobile number",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User selects \"\u003cGender\u003e\" in gender",
+  "name": "User edit \"\u003cGender\u003e\" in gender",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User selects \"\u003cDepartment\u003e\" in department",
+  "name": "User edit \"\u003cDepartment\u003e\" in department dropdown",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User enters \"\u003cBirth Date\u003e\" in birth date",
+  "name": "User edit \"\u003cBirthDate\u003e\" in birth date calendar",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User enters \"\u003cSalary\u003e\" in salary",
+  "name": "User edit \"\u003cSalary\u003e\" in salary",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User selects \"\u003cBatch\u003e\" in batch",
+  "name": "User edit \"\u003cBatch\u003e\" in batch",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User enters \"\u003cSection\u003e\" in section",
+  "name": "User edit \"\u003cSection\u003e\" in section",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User enters \"\u003cPermanent Address\u003e\" in permanent address",
+  "name": "User edit \"\u003cPresentAdress\u003e\" in present adress",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User clicks on Submit button",
+  "name": "User click Submit button",
   "keyword": "And "
 });
 formatter.step({
-  "name": "User should be able to see created teacher displayed in the UI",
+  "name": "User should see the edited profile displayed in the UI",
   "keyword": "Then "
 });
 formatter.step({
-  "name": "Tester should be able to verify created teacher in the Database",
+  "name": "Tester should be able to veryfy the edited Teacher in DataBase",
   "keyword": "And "
 });
 formatter.examples({
@@ -102,44 +102,44 @@ formatter.examples({
   "rows": [
     {
       "cells": [
-        "First name",
-        "Last name",
+        "Firstname",
+        "Lastname",
         "Email",
-        "Joining Date",
+        "JoiningDate",
         "Password",
         "Subject",
-        "Mobile number",
+        "MobileNumber",
         "Gender",
         "Department",
-        "Birth Date",
+        "BirthDate",
         "Salary",
         "Batch",
         "Section",
-        "Permanent Address"
+        "PresentAdress"
       ]
     },
     {
       "cells": [
-        "Dwight",
-        "Schrute",
-        "dwightschrute@dundermifflin.com",
-        "01/01/2000",
-        "6789",
-        "Business",
-        "8475555678",
-        "Male",
-        "English",
-        "08/15/1980",
-        "80000",
-        "12",
-        "Paper",
-        "Scranton, PA"
+        "Macarena",
+        "John",
+        "mcjo@xxx.com",
+        "01/01/2020",
+        "43212",
+        "HR",
+        "0987654321",
+        "Female",
+        "Science",
+        "01/01/2001",
+        "125 000",
+        "2",
+        "Auto",
+        "2ndAvenue"
       ]
     }
   ]
 });
 formatter.scenario({
-  "name": "Verify Create Teacher Functionality",
+  "name": "Verify Update Teacher functionality",
   "description": "",
   "keyword": "Scenario Outline",
   "tags": [
@@ -147,7 +147,7 @@ formatter.scenario({
       "name": "@COD-279"
     },
     {
-      "name": "@COD-278"
+      "name": "@COD-274"
     }
   ]
 });
@@ -155,191 +155,191 @@ formatter.before({
   "status": "passed"
 });
 formatter.step({
-  "name": "User clicks on Teachers module",
+  "name": "User click on threedots button on Christian teacher",
   "keyword": "When "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_clicks_on_Teachers_module()"
+  "location": "EditTeacher_StepDefs.user_click_on_threedots_button_on_Christian_teacher()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User clicks on Add Teacher",
+  "name": "User click on Edit button",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_clicks_on_Add_Teacher()"
+  "location": "EditTeacher_StepDefs.userClickOnEditButton()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User enters \"Dwight\" in first name",
+  "name": "User edit \"Macarena\" in first name",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_enters_in_first_name(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_first_name(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User enters \"Schrute\" in last name",
+  "name": "User edit \"John\" in last name",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_enters_in_last_name(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_last_name(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User enters \"dwightschrute@dundermifflin.com\" in email",
+  "name": "User edit \"mcjo@xxx.com\" in email",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_enters_in_email(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_email(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User enters \"01/01/2000\" in joining date",
+  "name": "User edit \"01/01/2020\" in joining date calendar",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_enters_in_joining_date(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_joining_date_calendar(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User enters \"6789\" in password",
+  "name": "User edit \"43212\" in password",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_enters_in_password(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_password(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User enters \"Business\" in subject",
+  "name": "User edit \"HR\" in subject",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_enters_in_subject(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_subject(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User enters \"8475555678\" in mobile number",
+  "name": "User edit \"0987654321\" in mobile number",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_enters_in_mobile_number(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_mobile_number(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User selects \"Male\" in gender",
+  "name": "User edit \"Female\" in gender",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_selects_in_gender(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_gender(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User selects \"English\" in department",
+  "name": "User edit \"Science\" in department dropdown",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_selects_in_department(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_department_dropdown(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User enters \"08/15/1980\" in birth date",
+  "name": "User edit \"01/01/2001\" in birth date calendar",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_enters_in_birth_date(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_birth_date_calendar(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User enters \"80000\" in salary",
+  "name": "User edit \"125 000\" in salary",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_enters_in_salary(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_salary(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User selects \"12\" in batch",
+  "name": "User edit \"2\" in batch",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_selects_in_batch(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_batch(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User enters \"Paper\" in section",
+  "name": "User edit \"Auto\" in section",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_enters_in_section(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_section(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User enters \"Scranton, PA\" in permanent address",
+  "name": "User edit \"2ndAvenue\" in present adress",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_enters_in_permanent_address(String)"
+  "location": "EditTeacher_StepDefs.user_edit_in_present_adress(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User clicks on Submit button",
+  "name": "User click Submit button",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_clicks_on_Submit_button()"
+  "location": "EditTeacher_StepDefs.user_click_Submit_button()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User should be able to see created teacher displayed in the UI",
+  "name": "User should see the edited profile displayed in the UI",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.user_should_be_able_to_see_created_teacher_displayed_in_the_UI()"
+  "location": "EditTeacher_StepDefs.user_should_see_the_edited_profile_displayed_in_the_UI()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "Tester should be able to verify created teacher in the Database",
+  "name": "Tester should be able to veryfy the edited Teacher in DataBase",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CreateTeacher_StepDefs.tester_should_be_able_to_verify_created_teacher_in_the_Database()"
+  "location": "EditTeacher_StepDefs.tester_should_be_able_to_veryfy_the_edited_Teacher_in_DataBase()"
 });
 formatter.result({
   "status": "passed"
